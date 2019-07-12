8410dddec59021556de135e50f31b583061a6d16
Anurag Sarkar <anurag.sarkar@oyorooms.com>
2019-07-12 09:39:02 +0530

    added scp

--- renderer/try.js
+++ renderer/try.js
@@ -1,13 +1,17 @@
+
+
 'use strict'
 
 const { ipcRenderer } = require('electron')
 
-document.getElementById('todoForm').addEventListener('submit', (evt) => {
+document.getElementById('employee-form').addEventListener('submit', (evt) => {
   // prevent default refresh functionality of forms
   evt.preventDefault()
-
   // input on the form
   const input = evt.target[0]
+  const input1 = evt.target[1]
+  console.log(input)
+  console.log(input1)
 
   // send todo to main process
   ipcRenderer.send('add-todo', input.value)
